97c6bfd5b89a516bbe10d9f33065b1925f79d3ae
LuanBPereira <152173403+LuanBPereira@users.noreply.github.com>
2024-04-25 11:50:12 -0300

    CONSEGUI FAZER O POST

--- src/main/java/br/com/kldoces/pacotes/controllers/CarrinhoDeComprasController.java
+++ src/main/java/br/com/kldoces/pacotes/controllers/CarrinhoDeComprasController.java
@@ -1,20 +1,36 @@
 package br.com.kldoces.pacotes.controllers;
 
-import br.com.kldoces.pacotes.models.Produtos;
 import br.com.kldoces.pacotes.services.CarrinhoDeCompras;
+import br.com.kldoces.pacotes.services.ItemDeCompra;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/carrinho")
+@RequestMapping("carrinho")
 public class CarrinhoDeComprasController {
 
     @Autowired
     private CarrinhoDeCompras cc;
 
     @PostMapping("/adicionar")
-    public void adicionarItemCarrinho(@RequestBody Produtos produto, @RequestParam int quantidade){
-        cc.adicionarItem(produto, quantidade);
+    public void adicionarItemCarrinho(@RequestBody ItemDeCompra itemDeCompra){
+        // log para mostrar se adicionou ou não
+        if (itemDeCompra.getProduto() == null) {
+            System.out.println("O produto é nulo. Não é possível adicionar ao carrinho.");
+            return;
+        } else {
+            System.out.println("produto adicionado no carrinho");
+        }
+
+        cc.adicionarItem(itemDeCompra.getProduto(), itemDeCompra.getQuantidade());
+
+    }
+
+    @GetMapping("/listar")
+    public List<ItemDeCompra> obterItensCarrinho() {
+        return cc.getItens();
     }
 
     @PostMapping("/limpar")
