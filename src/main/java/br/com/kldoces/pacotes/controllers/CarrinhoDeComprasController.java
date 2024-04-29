8689a84dbc88d9a6a11fd75abf6adaf49bde2e01
LuanBPereira <152173403+LuanBPereira@users.noreply.github.com>
2024-04-28 23:06:02 -0300

    e

--- src/main/java/br/com/kldoces/pacotes/controllers/CarrinhoDeComprasController.java
+++ src/main/java/br/com/kldoces/pacotes/controllers/CarrinhoDeComprasController.java
@@ -21,21 +21,30 @@ public class CarrinhoDeComprasController {
             System.out.println("O produto é nulo. Não é possível adicionar ao carrinho.");
             return;
         } else {
-            System.out.println("produto adicionado no carrinho");
+            System.out.println("produto adicionado no carrinho com sucesso!");
         }
 
         cc.adicionarItem(itemDeCompra.getProduto(), itemDeCompra.getQuantidade());
 
     }
 
+    @PostMapping("/valorTotal")
+    public double calculoTotal(){
+        double valorTotal = cc.calcularTotal();
+        return valorTotal;
+    }
+
     @GetMapping("/listar")
     public List<ItemDeCompra> obterItensCarrinho() {
-        return cc.getItens();
+        var listar = cc.getItens();
+        System.out.println("Itens listado com sucesso!");
+        return listar;
     }
 
     @PostMapping("/limpar")
     public void limparCarrinho(){
         cc.limparCarrinho();
+        System.out.println("Carrinho foi limpado com sucesso!");
     }
 
 }
